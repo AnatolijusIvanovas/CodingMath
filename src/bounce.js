2bfe51a8269c01ac4f1d2ee7e54bb250972fb474
Anatolijus <join.eu.pn@gmail.com>
2017-04-21 21:40:12 +0300

    Updated bounce.js

--- src/bounce.js
+++ src/bounce.js
@@ -3,7 +3,7 @@ window.onload = function() {
         context = canvas.getContext("2d"),
         width = canvas.width = window.innerWidth,
         height = canvas.height = window.innerHeight;
-    var p = particle.create(width / 2, height / 2, 0, Math.random() * Math.PI * 2, 0);
+    var p = particle.create(width / 2, height / 2, 5, Math.random() * Math.PI * 2, 0);
     p.color = '#123456';
     p.radius = 40;
     update();
@@ -14,6 +14,22 @@ window.onload = function() {
         context.fillStyle = p.color;
         context.arc(p.position.getX(), p.position.getY(), p.radius, 0, Math.PI * 2, false);
         context.fill();
+        if (p.position.getX() + p.radius > width) {
+            p.position.setX(width - p.radius);
+            p.velocity.setX(-p.velocity.getX());
+        }
+        if (p.position.getX() - p.radius < 0) {
+            p.position.setX(p.radius);
+            p.velocity.setX(-p.velocity.getX());
+        }
+        if (p.position.getY() + p.radius > height) {
+            p.position.setY(height - p.radius);
+            p.velocity.setY(-p.velocity.getY());
+        }
+        if (p.position.getY() - p.radius < 0) {
+            p.position.setY(p.radius);
+            p.velocity.setY(-p.velocity.getY());
+        }
         requestAnimationFrame(update);
     }
 };
