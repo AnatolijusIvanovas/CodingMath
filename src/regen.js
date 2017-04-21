5e34f4d53c30c48c3d3ca62784e50e3769e4beb3
Anatolijus <join.eu.pn@gmail.com>
2017-04-21 18:53:46 +0300

    Updated regen.js

--- src/regen.js
+++ src/regen.js
@@ -6,7 +6,8 @@ window.onload = function() {
 	var particles = [];
 	for (var i = 0; i < 500; i++) {
 	    var p = particle.create(Math.random() * width, height, Math.random() * 8 + 5, -Math.PI / 2 + (Math.random() * .4 - .2), 0.1);
-	    p.radius = Math.random() * 9 + 1;
+	    p.color = "#"+((1<<24)*Math.random()|0).toString(16);
+	    p.radius = Math.random() * 5 + 5;
 	    particles.push(p);
 	}
 	update();
@@ -16,6 +17,7 @@ window.onload = function() {
 		    var p = particles[i];
 		    p.update();
 		    context.beginPath();
+		    context.fillStyle = p.color;
 		    context.arc(p.position.getX(), p.position.getY(), p.radius, 0, Math.PI * 2, false);
 		    context.fill();
 		}
@@ -25,10 +27,15 @@ window.onload = function() {
 	function regen() {
 	    for (var i = 0; i < particles.length; i += 1) {
 	        var p = particles[i];
-	        if (p.position.getY() - p.radius > height) {
+	        if (p.position.getX() - p.radius > width ||
+                p.position.getX() + p.radius < 0 ||
+	            p.position.getY() - p.radius > height ||
+	            p.position.getY() + p.radius < 0)
+	        {
 	            p.position.setX(Math.random() * width);
 	            p.position.setY(height);
-	            p.radius = Math.random() * 9 + 1;
+	            p.color = "#"+((1<<24)*Math.random()|0).toString(16);
+	            p.radius = Math.random() * 5 + 5;
 	            p.velocity.setLength(Math.random() * 8 + 5);
 	            p.velocity.setAngle(-Math.PI / 2 + (Math.random() * .4 - .2));
 	        }
